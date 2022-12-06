87154a37e4fb42e355cdcd3a251bb88415dd23b5
jusbro <97056196+jusbro@users.noreply.github.com>
2022-12-06 14:36:56 -0500

    Update SpectraPlotter.py

--- SpectraPlotter.py
+++ SpectraPlotter.py
@@ -5,7 +5,7 @@ import csv
 x = []
 y = []
   
-with open('AM15GSpectra.csv','r') as csvfile:
+with open('sunSpectrum.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter = ',')
       
     for row in plots:
@@ -13,10 +13,17 @@ with open('AM15GSpectra.csv','r') as csvfile:
         y.append(float(row[1]))
 plt.style.use('dark_background') 
 plt.plot(x, y, color = 'r', linestyle=":", linewidth=2)
-plt.xticks(np.arange(0, 200, 4))
+plt.xticks(np.arange(1.8, 900, 41))
 #plt.yticks(np.arange(0, 3, 1))
 plt.xlabel('Wavelength')
 plt.ylabel('Relative Intensity')
 plt.title('Solar Spectrum 12/5/22')
-plt.annotate('Point', xy=(930,0.2), xytext = (900,0))
+#plt.xticks([300,350,400,450,500,550,600,650,700,750,800,850,900])
+#plt.annotate('H', xy=(656.3,0.2), xytext = (630,0.3), arrowprops=dict(facecolor='green', shrink=0.05))
+#Hydrogen Alpha
+#plt.axvline(306.3, color = 'white', label="Hydrogen", linestyle = "--")
+#Calcium II
+#plt.axvline(396.8, color = 'white', label="Calcium II", linestyle = "--")
+#Sodium I
+plt.axvline(589.6, color = 'white', label="Sodium I", linestyle = "--")
 plt.show()
